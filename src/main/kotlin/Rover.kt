0c30d299fffe49c63d17e66db1e072adc563e584
acunap <hello@antonioacuna.dev>
2023-03-29 19:09:54 +0200

    Refactor Rovers functionality

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -6,41 +6,50 @@ class Rover {
     fun processCommands(commands: String): String {
         for (command in commands.split("")) {
             when (command) {
-                "L" -> {
-                    when (direction) {
-                        "N" -> direction = "W"
-                        "W" -> direction = "S"
-                        "S" -> direction = "E"
-                        "E" -> direction = "N"
-                    }
-                }
-                "R" -> {
-                    when (direction) {
-                        "N" -> direction = "E"
-                        "E" -> direction = "S"
-                        "S" -> direction = "W"
-                        "W" -> direction = "N"
-                    }
-                }
-                "M" -> {
-                    when (direction) {
-                        "N" -> y++
-                        "E" -> x++
-                        "S" -> y--
-                        "W" -> x--
-                    }
-                }
-                "B" -> {
-                    when (direction) {
-                        "N" -> y--
-                        "E" -> x--
-                        "S" -> y++
-                        "W" -> x++
-                    }
-                }
+                "L" -> rotateLeft()
+                "R" -> rotateRight()
+                "M" -> moveForwards()
+                "B" -> moveBackwards()
             }
         }
 
         return "${x}.${y}.${direction}"
+    }
+
+    private fun rotateLeft() {
+        when (direction) {
+            "N" -> direction = "W"
+            "W" -> direction = "S"
+            "S" -> direction = "E"
+            "E" -> direction = "N"
+        }
+    }
+
+    private fun rotateRight() {
+        when (direction) {
+            "N" -> direction = "E"
+            "E" -> direction = "S"
+            "S" -> direction = "W"
+            "W" -> direction = "N"
+        }
+    }
+
+    private fun moveForwards() {
+        when (direction) {
+            "N" -> y++
+            "E" -> x++
+            "S" -> y--
+            "W" -> x--
+            else -> {}
+        }
+    }
+
+    private fun moveBackwards() {
+        when (direction) {
+            "N" -> y--
+            "E" -> x--
+            "S" -> y++
+            "W" -> x++
+        }
     }
 }
